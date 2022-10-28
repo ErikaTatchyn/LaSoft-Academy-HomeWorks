9c2d27b5a5fc76ae3b07a15dcfda027421c1fb6c
Erika Tatchyn <erika.ucu@gmail.com>
2022-10-28 09:41:55 +0100

    fix hw3

--- homework3/task3.js
+++ homework3/task3.js
@@ -7,8 +7,8 @@
 // [1, 3, 4, 5, 7]
 
 function stringToArr(str) {
-  let arrOfStr = [];
-  let arrOfNum = [];
+  const arrOfStr = [];
+  const arrOfNum = [];
   const splittedStr = str.split(",");
   for (item of splittedStr) {
     if (!isNaN(item)) {
