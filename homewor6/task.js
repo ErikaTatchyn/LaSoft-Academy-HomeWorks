b8dfba56925e27ba87b0b400cb27cb9165acb89c
Erika Tatchyn <erika.ucu@gmail.com>
2022-11-09 12:56:42 +0000

    fix comments

--- homewor6/task.js
+++ homewor6/task.js
@@ -1,13 +1,15 @@
 function quadraticEquation(a, b, c) {
-  const x1 = (-1 * b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
-  const x2 = (-1 * b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
-
   try {
     if (!c) throw "missing parameter";
     if (a === 0) throw "not a quadratic equation";
   } catch (err) {
-    console.log(err);
+    throw new ReferenceError(err);
   }
+
+  const x1 =
+    (-1 * b + Math.sqrt(Math.abs(Math.pow(b, 2) - 4 * a * c))) / (2 * a);
+  const x2 =
+    (-1 * b - Math.sqrt(Math.abs(Math.pow(b, 2) - 4 * a * c))) / (2 * a);
 
   return `x1=${x1}, x2=${x2}`;
 }
@@ -15,3 +17,4 @@ function quadraticEquation(a, b, c) {
 quadraticEquation(1, 5, 4); // x1=-1, x2=-4
 quadraticEquation(1, 5); //missing parameter
 quadraticEquation(0, 1, 5); //not a quadratic equation
+quadraticEquation(1, 1, 1);
